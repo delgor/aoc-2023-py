58c479f51dc685a434a4ff4ac77b77bec7e13498
Alexander Siol <alex+aoc2023py@hackthehell.de>
2023-12-09 13:30:49 +0100

    Solution for day 9, part 2

--- 9/combined.py
+++ 9/combined.py
@@ -16,6 +16,7 @@ def row_all_zero(row):
 
 
 sum_p1 = 0
+sum_p2 = 0
 
 for line in input:
     print(line)
@@ -31,15 +32,29 @@ for line in input:
 
         depth += 1
 
+    max_depth = depth
+
     # p1: extrapolate forward
     while depth > 0:
         next_elem = rows[depth - 1][-1] + rows[depth][-1]
         rows[depth - 1].append(next_elem)
         depth -= 1
 
+    sum_p1 += rows[0][-1]
+
+    # p2: extrapolate backwards
+    depth = max_depth
+    while depth > 0:
+        next_elem = rows[depth - 1][0] - rows[depth][0]
+        rows[depth - 1].insert(0, next_elem)
+        depth -= 1
+
+    sum_p2 += rows[0][0]
+
+    # debug print
     for row in rows.values():
         print(row)
 
-    sum_p1 += rows[0][-1]
 
 print(f"sum_p1: {sum_p1}")
+print(f"sum_p2: {sum_p2}")
