204c902805d658c641644dd1fc91ef6add212406
Alexander Siol <alex+aoc2023py@hackthehell.de>
2023-12-18 20:57:01 +0100

    Solution for day 16, part 2

--- 16/combined.py
+++ 16/combined.py
@@ -34,10 +34,11 @@ def print_pattern(grid):
         print(f"{row_str}")
 
 
-energize_grid_split_check = []
 
-
-def energize_grid(beam_start: vec, beam_dir: vec, grid, energy_grid):
+def energize_grid(beam_start: vec, beam_dir: vec, grid, energy_grid, depth=0, energize_grid_split_check=None):
+    print = lambda x: None
+    if depth == 0:
+        energize_grid_split_check = []
     pos = beam_start
     print(f"Energize from {beam_start} moving {beam_dir}")
     while 0 <= pos.y < len(grid) and 0 <= pos.x < len(grid[0]):
@@ -81,10 +82,10 @@ def energize_grid(beam_start: vec, beam_dir: vec, grid, energy_grid):
                     energize_grid_split_check.append(pos)
 
                     new_dir_a = beam_dir.rotate_cw()
-                    energize_grid(pos + new_dir_a, new_dir_a, grid, energy_grid)
+                    energize_grid(pos + new_dir_a, new_dir_a, grid, energy_grid, depth+1, energize_grid_split_check)
 
                     new_dir_b = beam_dir.rotate_ccw()
-                    energize_grid(pos + new_dir_b, new_dir_b, grid, energy_grid)
+                    energize_grid(pos + new_dir_b, new_dir_b, grid, energy_grid, depth+1, energize_grid_split_check)
                 break
 
         elif obj == "|":
@@ -100,10 +101,10 @@ def energize_grid(beam_start: vec, beam_dir: vec, grid, energy_grid):
                     energize_grid_split_check.append(pos)
 
                     new_dir_a = beam_dir.rotate_cw()
-                    energize_grid(pos + new_dir_a, new_dir_a, grid, energy_grid)
+                    energize_grid(pos + new_dir_a, new_dir_a, grid, energy_grid, depth+1, energize_grid_split_check)
 
                     new_dir_b = beam_dir.rotate_ccw()
-                    energize_grid(pos + new_dir_b, new_dir_b, grid, energy_grid)
+                    energize_grid(pos + new_dir_b, new_dir_b, grid, energy_grid, depth+1, energize_grid_split_check)
                 break
         else:
             raise RuntimeError("Unknown object")
@@ -119,6 +120,16 @@ def filter_energy_grid_energized(energy_grid):
                 energized_count += 1
     return energized_grid, energized_count
 
+
+def energized_count_by_start(beam_start: vec, beam_dir: vec, grid):
+    energy_grid = [[0] * len(grid[0]) for row in grid]
+    energize_grid(beam_start, beam_dir, grid, energy_grid)
+
+    energized_grid, energized_count = filter_energy_grid_energized(energy_grid)
+    #print(f"count {energized_count}")
+    #print_pattern(energized_grid)
+    #print()
+    return energized_count
 
 ## Application
 p = Path("input.real.txt")
@@ -140,12 +151,43 @@ if len(current_pattern):
 grid = all_patterns[0]
 
 ## Part 1
-print_pattern(grid)
-print()
+#print_pattern(grid)
+#print()
 
-energy_grid = [[0] * len(grid[0]) for row in grid]
-energize_grid(vec(0, 0), vec(0, 1), grid, energy_grid)
+print(f"Total of energized tiles: {energized_count_by_start(vec(0,3), vec(1,0), grid)}")
+print(f"Total of energized tiles: {energized_count_by_start(vec(0,0), vec(0,1), grid)}")
 
-energized_grid, energized_count = filter_energy_grid_energized(energy_grid)
-print_pattern(energized_grid)
-print(f"Total of energized tiles: {energized_count}")
+
+## Part 2
+best_count = 0
+best_start = None
+best_dir = None
+
+def update_best(start, dir, grid):
+    global best_count, best_start, best_dir
+    new_count = energized_count_by_start(start, dir, grid)
+    print(f"Checking {start} going {dir}, reached {new_count}")
+    if new_count > best_count:
+        best_start = start
+        best_dir = dir
+        best_count = new_count
+
+for row_idx in range(len(grid)):
+    start = vec(row_idx, 0)
+    dir = vec(0, 1)
+    update_best(start, dir, grid)
+
+    start = vec(row_idx, len(grid[0])-1)
+    dir = vec(0, -1)
+    update_best(start, dir, grid)
+
+for col_idx in range(len(grid[0])):
+    start = vec(0, col_idx)
+    dir = vec(1, 0)
+    update_best(start, dir, grid)
+
+    start = vec(len(grid) - 1, col_idx)
+    dir = vec(-1, 0)
+    update_best(start, dir, grid)
+
+print(f"Best start at {best_start} going {best_dir}, reaches {best_count}")
